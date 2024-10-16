59fc0c6f21a9edbea98fc2f0203a59f9f4dbefe3
mtaltunkanat <muhammedaltunkanat.kariyer@gmail.com>
2024-10-16 17:00:33 +0300

    First Commit

---
+++ src/components/header/homes/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+     <div className="homeContainer">
+        {items.map((item) =>{
+            ;<HomeCard />
+        })}
+        </div> 
+    </>
+  )
+}
+
+export default Home
---
+++ src/home/HomePages.jsx
@@ -0,0 +1,14 @@
+import React ,{useState} from 'react'
+import Homes from '../components/header/homes/Homes'
+
+
+export const HomePages = () => {
+    
+    return( 
+    <>
+     <Homes />
+      </>
+      )
+   }     
+    
+
